f1d5568356ac66741149943bef2e0f0a15cfa022
centerRyo <abc.atrn@gmail.com>
2020-08-08 12:29:43 +0900

    ユーザーテーブルを作成した

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,11 +14,16 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->bigIncrements('id');
-            $table->string('name');
-            $table->string('email')->unique();
+            $table->bigIncrements('id')->comment('ユーザーID');
+            $table->string('name')->nullable()->comment('ユーザー名');
+            $table->string('email', 255)->unique();
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->string('password', 255);
+            $table->integer('gender')->nullable()->comment('男性：0 女性：1 その他：2');
+            $table->integer('age')->nullable()->comment('年齢');
+            $table->string('job', 255)->nullable()->comment('職業');
+            $table->string('role', 255)->nullable()->comment('職種');
+            $table->string('pic', 255)->nullable()->comment('アイコン画像');
             $table->rememberToken();
             $table->timestamps();
         });
