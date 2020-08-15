97d9bec0a8359739dae4e0bd92b8b7204cd5e880
centerRyo <abc.atrn@gmail.com>
2020-08-15 16:27:39 +0900

    id=appはここではいらないので削除

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,7 +10,7 @@
     <title>{{ config('app.name', 'Laravel') }}</title>
 
     <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
+    <script src="{{ asset('js/app.js') }}" defer></script>                                            
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
@@ -21,51 +21,49 @@
     <link href="{{ asset('css/reset.css') }}" rel="stylesheet">
 </head>
 <body>
-    <div id="app">
-        <header class="header">
-            <div class="site-width">
-                <h1 class="header__title"><a href=""><img src="{{ asset('/img/logo3.png') }}" alt=""></a></h1>
+    <header class="header">
+        <div class="site-width">
+            <h1 class="header__title"><a href=""><img src="{{ asset('/img/logo3.png') }}" alt=""></a></h1>
 
-                <nav>
-                    <ul class="menu">
-                        @guest
+            <nav>
+                <ul class="menu">
+                    @guest
+                        <li class="menu__item">
+                            <a class="menu__item_link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                        </li>
+                        @if (Route::has('register'))
                             <li class="menu__item">
-                                <a class="menu__item_link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                                <a class="menu__item_link" href="{{ route('register') }}">{{ __('Register') }}</a>
                             </li>
-                            @if (Route::has('register'))
-                                <li class="menu__item">
-                                    <a class="menu__item_link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                                </li>
-                            @endif
-                        @else
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }} <span class="caret"></span>
+                        @endif
+                    @else
+                        <li class="nav-item dropdown">
+                            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                {{ Auth::user()->name }} <span class="caret"></span>
+                            </a>
+
+                            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                <a class="dropdown-item" href="{{ route('logout') }}"
+                                    onclick="event.preventDefault();
+                                                    document.getElementById('logout-form').submit();">
+                                    {{ __('Logout') }}
                                 </a>
 
-                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
-                                    </a>
-
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                        @csrf
-                                    </form>
-                                </div>
-                            </li>
-                        @endguest 
-                    </ul>
-                </nav>
-            </div>
-        </header>
-        <main class="container_wrapper">
-            @yield('content')
-        </main>
-        <footer class="footer">
-            Copyright - Sitell 2020, All Right Reserved.
-        </footer>
-    </div>
+                                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                    @csrf
+                                </form>
+                            </div>
+                        </li>
+                    @endguest 
+                </ul>
+            </nav>
+        </div>
+    </header>
+    <main class="container_wrapper">
+        @yield('content')
+    </main>
+    <footer class="footer">
+        Copyright - Sitell 2020, All Right Reserved.
+    </footer>
 </body>
 </html>
