837b34a61e027b3b315e5873eafc1c6f3f0c119e
centerRyo <abc.atrn@gmail.com>
2020-08-16 15:59:00 +0900

    論文登録画面でカテゴリーの値を取得できるようにした

--- app/Http/Controllers/RonbunController.php
+++ app/Http/Controllers/RonbunController.php
@@ -2,12 +2,34 @@
 
 namespace App\Http\Controllers;
 
+use App\Category;
+use App\Ronbun;
 use Illuminate\Http\Request;
 
 class RonbunController extends Controller
 {
     public function new()
     {
-        return view('ronbun.new');
+        $categories = Category::select('name')->get()->all();
+        $category_names = [];
+        foreach ($categories as $category) {
+            $category_names[] = $category->name;
+        }
+        return view('ronbun.new', compact('category_names'));
+    }
+
+    public function create(Request $request)
+    {
+        $request->validate([
+            'title' => 'required|string|max:255',
+            'category_id' => 'required',
+            'author' => 'required|string|max:255',
+            'abstract' => 'required',
+        ]);
+
+        $ronbun = new Ronbun();
+        $ronbun->fill($request->all())->save();
+
+        return redirect('/mypage')->with('flash_message', __('Registered!'));
     }
 }
