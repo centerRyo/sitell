707e4ab80ceae375c0f5dd55e5cf3d60b04fa006
centerRyo <abc.atrn@gmail.com>
2020-08-16 19:58:17 +0900

    プロフィール部分のスタイルを作成

--- resources/views/mypage.blade.php
+++ resources/views/mypage.blade.php
@@ -1,23 +1,47 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
+    <section class="profileContainer">
+        <div class="site-width">
+            <section class="profile">
+                <div class="profile__img"></div>
+                <div class="profile__info">
+                    <h2 class="profile__name"></h2>
+                    <div class="sns">
+                        <a href=""><i class="fab fa-facebook-square"></i></a>
+                        <a href=""><i class="fab fa-twitter-square"></i></a>
+                        <a href=""><i class="fab fa-instagram-square"></i></a>
+                    </div>
                 </div>
-            </div>
+            </section>
         </div>
+    </section>
+
+    <div class="site-width">
+        <section class="mainContainer">
+            <section class="contents">
+                <h2 class="contents__title">登録論文一覧</h2>
+                <div class="contents__container">
+                    <div class="content">
+                        <a href="">
+                            <img src="" alt="">
+                            <p></p>
+                        </a>
+                    </div>
+                </div>
+            </section>
+
+            <section class="contents">
+                <h2 class="contents_title">お気に入り登録一覧</h2>
+                <div class="contents__container">
+                    <div class="content">
+                        <a href="">
+                            <img src="" alt="">
+                            <p></p>
+                        </a>
+                    </div>
+                </div>
+            </section>
+        </section>
     </div>
-</div>
 @endsection
