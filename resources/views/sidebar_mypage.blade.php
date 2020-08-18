6e9b97c2b9a10206b2390728d9b3691a3e3763a6
centerRyo <abc.atrn@gmail.com>
2020-08-18 22:45:36 +0900

    論文投稿ページへのルートを設定

--- resources/views/sidebar_mypage.blade.php
+++ resources/views/sidebar_mypage.blade.php
@@ -1,5 +1,5 @@
 <section class="sidebarMypage">
-  <a href="">論文を投稿する</a>
+  <a href="{{ route('ronbun.new') }}">論文を投稿する</a>
   <a href="">プロフィールを編集する</a>
   <a href="">パスワードを変更する</a>
   <a href="">退会する</a>
