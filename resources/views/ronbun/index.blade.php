542ef2f43947bd6d2383cb95434b3af61d9304cc
centerRyo <abc.atrn@gmail.com>
2020-09-04 22:30:53 +0900

    新着論文を表示する

--- resources/views/ronbun/index.blade.php
+++ resources/views/ronbun/index.blade.php
@@ -10,13 +10,25 @@
       <section class="contents">
         <h2 class="contents__title">新着論文一覧</h2>
         <div class="contents__container">
+          @foreach ($ronbuns as $ronbun)
           <div class="content">
             <a href="" class="content__link">
-              <p class="content__title"></p>
+              @if (isset($ronbun->thumbnail))
+                <img src="/uploads/{{ $ronbun->thumbnail }}" class="content__image">
+              @else
+                <img src="{{ asset('/img/no_image.png') }}" class="content__image">
+              @endif
+              <p class="content__title">{{ $ronbun->title }}</p>
             </a>
+            <span class="content__category">{{ $ronbun->category_name }}</span>
           </div>
+          @endforeach
         </div>
+
+        <a href="" class="leadLink">新着論文一覧を見る</a>
       </section>
     </section>
+
+    @include('ronbun.sidebar')
   </div>
 @endsection
