8d94f6d409ab1174267ffc508fe548d908dfd8d4
centerRyo <abc.atrn@gmail.com>
2020-09-06 13:49:54 +0900

    /ronbun/{id}より上のルーティングの定義じゃないと数字じゃないとされてだめになる

--- routes/web.php
+++ routes/web.php
@@ -17,10 +17,10 @@ Auth::routes();
 
 Route::get('/', 'RonbunController@index')->name('ronbun.index');
 Route::get('/latest', 'RonbunController@latest')->name('ronbun.latest');
-Route::get('/ronbun/{id}', 'RonbunController@show')->name('ronbun.show');
-Route::get('/mypage', 'UserController@mypage')->name('mypage');
 Route::get('/ronbun/new', 'RonbunController@new')->name('ronbun.new');
 Route::post('/ronbun/new', 'RonbunController@create');
+Route::get('/ronbun/{id}', 'RonbunController@show')->name('ronbun.show');
+Route::get('/mypage', 'UserController@mypage')->name('mypage');
 Route::get('/withdraw', 'UserController@withdraw')->name('withdraw');
 Route::post('/withdraw', 'UserController@delete')->name('user.delete');
 Route::get('/user/edit', 'UserController@edit')->name('user.edit');
