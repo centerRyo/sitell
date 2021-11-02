633629036c3ec45bb660bebdcf9961fd6988677f
centerRyo <abc.atrn@gmail.com>
2021-11-02 20:58:32 +0900

    フッターcomponent作成

---
+++ resources/ts/ui/src/presentation/ui-o-footer/DomFooter.tsx
@@ -0,0 +1,54 @@
+import React, { ReactNode } from 'react'
+import { BsTwitter, BsFacebook, BsYoutube } from 'react-icons/bs'
+import { LogoInverted } from '../../assets/LogoInverted'
+import defaultClassNames from './dup-footer-default.m.scss'
+
+type Props = {
+  linksLeft: Array<ReactNode>
+  linksRight: Array<ReactNode>
+  classNames?: string
+}
+
+export const DomFooter: React.FC<Props> = ({
+  linksLeft,
+  linksRight,
+  classNames
+}) => {
+  const styles = classNames || defaultClassNames
+
+  return (
+    <footer className={styles.footer}>
+      <div className={styles.root}>
+        <div className={styles.container}>
+          <a className={styles.icon} href="/">
+            <LogoInverted />
+          </a>
+          <div className={styles.links}>
+            <ul className={styles.items}>
+              {linksLeft.map((link, index) => (
+                <li className={styles.item} key={index}>
+                  {link}
+                </li>
+              ))}
+            </ul>
+            <ul className={styles.items}>
+              {linksRight.map((link, index) => (
+                <li className={styles.item} key={index}>
+                  {link}
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
+        <div className={styles.bottom}>
+          <div className={styles.social}>
+            <a href="https://twitter.com/xt_rynkd08" className={styles.social__item}><BsTwitter /></a>
+            <a href="https://www.facebook.com/profile.php?id=100012703666212" className={styles.social__item}><BsFacebook /></a>
+            <a href="{/* sitell YoutubeチャンネルができたらURLを入れる */}" className={styles.social__item}><BsYoutube /></a>
+          </div>
+          <p className={styles.copyright}>Copyrights©︎xtimmune. All Rights Reserved.</p>
+        </div>
+      </div>
+    </footer>
+  )
+}
---
+++ resources/ts/ui/src/presentation/ui-o-footer/DupFooter.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { DupLink, DupLinkTypes } from '../ui-a-link/DupLink'
+import { DomFooter } from './DomFooter'
+
+export const DupFooter: React.FC = () => {
+  // 命名ダサい
+  const linksLeft = [
+    <DupLink type={DupLinkTypes.About} />,
+    <DupLink type={DupLinkTypes.Company} />,
+    <DupLink type={DupLinkTypes.Contact} />,
+    <DupLink type={DupLinkTypes.FAQ} />,
+  ]
+  const linksRight = [
+    <DupLink type={DupLinkTypes.TermsOfUse} />,
+    <DupLink type={DupLinkTypes.PrivacyPolicy} />,
+  ]
+  return (
+    <DomFooter
+      linksLeft={linksLeft}
+      linksRight={linksRight}
+    />
+  )
+}
