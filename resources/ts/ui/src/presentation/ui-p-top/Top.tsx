71e950cc9d9426963609000f3bc3a9832fe95280
centerRyo <abc.atrn@gmail.com>
2021-11-17 15:54:09 +0900

    グリッドカードcomponent作成

--- resources/ts/ui/src/presentation/ui-p-top/Top.tsx
+++ resources/ts/ui/src/presentation/ui-p-top/Top.tsx
@@ -5,6 +5,7 @@ import { DupLabelCategory } from '../ui-a-label/DupLabelCategory'
 import { DupTab } from '../ui-c-tab/DupTab'
 import { DupTitle, DupTitleTypes } from '../ui-c-title/DupTitle'
 import { DomCard } from '../ui-m-card/DomCard'
+import { DomCardGrid } from '../ui-m-card/DomCardGrid'
 import { DupFooter } from '../ui-o-footer/DupFooter'
 import { DomHeader } from '../ui-o-header/DomHeader'
 
@@ -19,6 +20,13 @@ export const Top: React.FC = () => {
         badge={{
           render: () => <DupBadge number={1} type={DupBadgeTypes.Gold} />
         }}
+        image="https://www.gstatic.com/webp/gallery3/1.png"
+        label={{
+          render: () => <DupLabelCard text="数学" />
+        }}
+        text="サンプルタイトルサンプルタイトルサンプルタイトル"
+      />
+      <DomCardGrid
         image="https://www.gstatic.com/webp/gallery3/1.png"
         label={{
           render: () => <DupLabelCard text="数学" />
