aa61bf7ac2a74ba6e2140b23bff053f59b2aaa98
centerRyo <abc.atrn@gmail.com>
2022-01-25 12:39:49 +0900

    api用のルーティング設定

--- routes/api.php
+++ routes/api.php
@@ -21,4 +21,5 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 Route::group(['middleware' => ['api']], function() {
     Route::resource('category/categoryList', 'Api\CategoryController', ['except' => ['create', 'edit']]);
     Route::get('ronbun/ronbunList', 'Api\RonbunController@index');
+    Route::get('ronbun/{id}', 'Api\RonbunController@show');
 });
