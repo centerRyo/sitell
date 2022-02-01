81a49ec946735d9344a306d00e17758593858df2
centerRyo <abc.atrn@gmail.com>
2022-02-01 22:33:55 +0900

    論文が更新日の降順で取得できる

--- app/Http/Controllers/Api/RonbunListController.php
+++ app/Http/Controllers/Api/RonbunListController.php
@@ -7,11 +7,29 @@ use Illuminate\Support\Facades\DB;
 
 class RonbunListController extends Controller
 {
+  public function index()
+  {
+    $ronbunList = $this->getLatestRonbunList();
+
+    return response()->json($ronbunList);
+  }
+
   public function category($category_id)
   {
     $ronbunList = $this->getCategoryRonbunList($category_id);
 
     return response()->json($ronbunList);
+  }
+
+  public function getLatestRonbunList()
+  {
+    $ronbunList = DB::table('ronbuns')
+                    ->join('categories', 'ronbuns.category_id', '=', 'categories.id')
+                    ->select('ronbuns.*', 'categories.name as category_name')
+                    ->orderBy('ronbuns.updated_at', 'desc')
+                    ->get();
+
+    return $ronbunList;
   }
 
   public function getCategoryRonbunList($id)
--- tests/Http/Controllers/Api/RonbunListControllerTest.php
+++ tests/Http/Controllers/Api/RonbunListControllerTest.php
@@ -10,6 +10,41 @@ use Tests\TestCase;
 class RonbunListControllerTest extends TestCase
 {
   /** @test */
+  public function 論文が更新日の降順で取得できる()
+  {
+    $category1 = $this->createCateogry();
+    $category2 = $this->createCateogry();
+    $this->createRonbun(
+      $title = 'テストタイトル1',
+      $author = 'テスト著者1',
+      $abstract = 'テストテキスト1テストテキスト1テストテキスト1テストテキスト1',
+      $category_id = $category1->id,
+    );
+    $this->createRonbun(
+      $title = 'テストタイトル2',
+      $author = 'テスト著者2',
+      $abstract = 'テストテキスト2テストテキスト2テストテキスト2テストテキスト2',
+      $category_id = $category2->id,
+    );
+    $this->createRonbun(
+      $title = 'テストタイトル3',
+      $author = 'テスト著者3',
+      $abstract = 'テストテキスト3テストテキスト3テストテキスト3テストテキスト3',
+      $category_id = $category1->id,
+    );
+    $this->createRonbun(
+      $title = 'テストタイトル4',
+      $author = 'テスト著者4',
+      $abstract = 'テストテキスト4テストテキスト4テストテキスト4テストテキスト4',
+      $category_id = $category2->id,
+    );
+
+    $ronbunList = (new RonbunListController)->getLatestRonbunList();
+
+    $this->assertEquals('テストタイトル4', $ronbunList->first()->title);
+  }
+
+  /** @test */
   public function 指定したカテゴリーIDに紐づく論文一覧が取得できる()
   {
     $category = $this->createCateogry();
