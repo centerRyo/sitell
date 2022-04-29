1a7f66ed7424175540d15fb083cd3ba7ffcaadee
centerRyo <abc.atrn@gmail.com>
2022-04-29 17:01:09 +0900

    change directory name presentation to components & entries to pages

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,10 +1,18 @@
-const mix = require('laravel-mix');
+const mix = require("laravel-mix");
 
-mix.ts('frontend/src/entries/app.tsx', 'public/js')
-   .sass('frontend/src/styles/app.scss', 'public/css')
-   .version()
-   .webpackConfig({
-      resolve: {
-         extensions: [ ".ts", ".tsx", ".sass", ".scss", ".css"]
-      }
-   });
+mix.ts("frontend/src/pages/app.tsx", "public/js")
+    .sass("frontend/src/styles/app.scss", "public/css")
+    .version()
+    .webpackConfig({
+        resolve: {
+            extensions: [
+                ".ts",
+                ".tsx",
+                ".sass",
+                ".js",
+                ".jsx",
+                ".scss",
+                ".css",
+            ],
+        },
+    });
